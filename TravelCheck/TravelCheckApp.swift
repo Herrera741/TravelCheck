f44dc29ddeadb81f955eaeab5dc02cc98b3f69b6
Sergio Herrera <sherrera725@gmail.com>
2024-06-23 00:49:48 -0700

    Initial Commit

---
+++ TravelCheck/TravelCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelCheckApp.swift
+//  TravelCheck
+//
+//  Created by Sergio Herrera on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
